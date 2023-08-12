c2f59f297ef10e6ce3ff92991209a62e0e7ed951
AmirBakhshi <amirbu051@gmail.com>
2023-08-12 13:51:09 +0330

    Add onControllerVisibilityChanged interface

--- app/src/main/java/com/tv/player/ImaPlayerActivity.kt
+++ app/src/main/java/com/tv/player/ImaPlayerActivity.kt
@@ -8,11 +8,11 @@ import com.google.ads.interactivemedia.v3.api.player.AdMediaInfo
 import com.google.ads.interactivemedia.v3.api.player.VideoAdPlayer
 import com.google.ads.interactivemedia.v3.api.player.VideoProgressUpdate
 import com.tv.core.base.TvPlayer
-import com.tv.core.util.mediaItems.MediaItem
-import com.tv.core.util.mediaItems.MediaQuality
 import com.tv.core.util.TvImaAdsLoader
 import com.tv.core.util.TvPlayerListener
+import com.tv.core.util.mediaItems.MediaItem
 import com.tv.core.util.mediaItems.MediaItemParent
+import com.tv.core.util.mediaItems.MediaQuality
 import com.tv.player.databinding.ActivityImaPlayerBinding
 import com.tv.player.util.UrlHelper
 
@@ -135,6 +135,11 @@ class ImaPlayerActivity : AppCompatActivity() {
         override fun onMediaListComplete(mediaItem: MediaItemParent) {
             super.onMediaListComplete(mediaItem)
             Log.i(TAG , "onMediaListComplete")
+        }
+
+        override fun onControllerVisibilityChanged(visibility: Int) {
+            super.onControllerVisibilityChanged(visibility)
+            Log.d(TAG, "onControllerVisibilityChanged: $visibility")
         }
     }
 
